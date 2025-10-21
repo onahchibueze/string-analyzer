5809d45b053b80e5dc271d83f5d8ee5587ec09ca
onahchibueze <chibuezesomotochukwu09@gmail.com>
2025-10-21 14:15:41 +0100

    port change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/strings", router);
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", async () => {
   await connectDB();
